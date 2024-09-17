ec0f9740a430e3727cdd74685e9f6655bd60d127
Jonnywang888 <trustmarketfollonica@gmail.com>
2024-09-17 10:29:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var mi,utente,floors,floorHeight,databiao,lis;
-var baseurl = 'http://trustmarket.ddnsfree.com/server/app.asp?mi=';
+var baseurl = 'https://trustmarket.ddnsfree.com/server/app.asp?mi=';
 var currentDate = new Date();
 var needload = true;
 var listmovimento = [];
