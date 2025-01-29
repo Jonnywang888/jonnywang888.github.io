3b7e5a172cb50771836183cb6c53ea8db289e6f1
Jonnywang888 <trustmarketfollonica@gmail.com>
2025-01-29 15:23:50 +0100

    1

--- code/js/register-service-worker.js
+++
@@ -1,9 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  const sw = 'service-worker.js'; // Required because parcel will not recognize this as a file and will throw during build.
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register(sw).catch(err => {
-      console.error(err);
-    });
-  }
-}
--- code/js/utils/log.js
+++
@@ -1,8 +0,0 @@
-/**
- * Log to console only in development mode.
- *
- * @param {...any} args - Arguments to log
- */
-export function log(...args) {
-  process.env.NODE_ENV === 'development' && console.log(...args);
-}
