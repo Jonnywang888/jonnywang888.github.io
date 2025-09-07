62191e09d623c7ee98ac6dfe0f03aa707fcb7373
Jonnywang888 <trustmarketfollonica@gmail.com>
2025-09-07 19:20:11 +0200

    1

--- src/app.js
+++ src/app.js
@@ -320,8 +320,6 @@ class Calendario {
             const ms = parseInt(dateString.slice(12, 13), 10) * 100;
 
             this.currentDate = new Date(year, month, day, hour, minute, second, ms);
-        } else {
-            this.currentDate = new Date();
         }
 
         // 设置 this.timeid
